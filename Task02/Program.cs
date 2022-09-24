ea9f4f3063cd0800cbb112f647661aedd61b4241
ram-xkr <ram-xkr@mail.ru>
2022-09-24 15:38:52 +0300

    Task 2

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает числа и выдаёт, какое число большее,
+//а какое меньшее.
+using static System.Console;
+
+Clear();
+WriteLine("Введите числа: ");
+int a = Convert.ToInt32(ReadLine());
+int b = Convert.ToInt32(ReadLine());
+if (a > b)
+{
+    WriteLine($"Наибольшее число равно {a}, а наименьшее равно {b}.");
+}
+else
+{
+    WriteLine($"Наибольшее число равно {b}, а наименьшее равно {a}.");
+}
